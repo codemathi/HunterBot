33c17f9f5c96e3e6b4026b54e67c36af2835ff04
assil4 <51454567+assil4@users.noreply.github.com>
2019-06-06 18:07:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ async def on_message_delete(message):
         await message.channel.send('Message deleted: ')
         await message.channel.send('{}: {}'.format(message.author.mention, message.content))
                 
-client.run('NTc1NjE2MzM0NzU1NTI4NzE0.XOW2mA.2fWt4kXExGP2VfKZZ8UliwNWj1A')
+client.run(BOT_TOKEN)
