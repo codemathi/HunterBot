d1ec978aec2eb6c7e229c552ef024a89e35334c1
assil4 <51454567+assil4@users.noreply.github.com>
2019-06-06 22:57:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,33 +5,33 @@ client = discord.Client()
 #Ready eveny
 @client.event
 async def on_ready():
-        print('We heve logged in as {0.user}'.format(client))
-        activity = discord.Game(name="Beta BOT v1")
-        await client.change_presence(status=discord.Status.idle, activity=activity)
+    print('We heve logged in as {0.user}'.format(client))
+    activity = discord.Game(name="Beta BOT v1")
+    await client.change_presence(status=discord.Status.idle, activity=activity)
 #Message on delete
 @client.event
 async def on_message_delete(message):
-    	if message.author == client.user:
-        	return
-
-    	await message.channel.send('[DELETED]{}: {}'.format(message.author.mention, message.content))
+        if message.author == client.user:
+            return
+        await message.channel.send('[DELETED]{}: {}'.format(message.author.mention, message.content))
 async def on_message(msg):
-		if message.author == client.user:
-			return
-        try:
-    		if mmsg.content.startwith('$!join'): 
-                if msg.voice_client is None:
-                    if msg.author.voice:
-    			         await msg.author.voice.channel.connect()
-                    else:
-                         await msg.channel.send('You are not connected to a voice channel.')
-        except:
-            await msg.channel.send('Aint')
+    if message.author == client.user:
+        return
+        
+    try:
+        if msg.content.startwith('$!join'): 
+            if msg.voice_client is None:
+                if msg.author.voice:
+                    await msg.author.voice.channel.connect()
+                else:
+                    await msg.channel.send('You are not connected to a voice channel.')
+    except:
+        await msg.channel.send('Aint')
 @client.event
 async def on_member_join(self, member):
-        guild = member.guild
-        if guild.system_channel is not None:
-            to_send = 'Welcome {0.mention} to {1.name}!'.format(member, guild)
-            await guild.system_channel.send(to_send)
-                
+    guild = member.guild
+    if guild.system_channel is not None:
+        to_send = 'Welcome {0.mention} to {1.name}!'.format(member, guild)
+        await guild.system_channel.send(to_send)
+        
 client.run(str(os.environ.get('BOT_TOKEN')))
