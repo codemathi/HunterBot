7aa5c6a69ae58e7bdd581bb36171bddc1a88943e
assil4 <51454567+assil4@users.noreply.github.com>
2019-06-06 18:33:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import discord
 import os
 
 client = discord.Client()
-
+#Ready eveny
 @client.event
 async def on_ready():
         print('We heve logged in as {0.user}'.format(client))
+        activity = discord.Game(name="$talk to me <3")
+    	await client.change_presence(status=discord.Status.idle, activity=activity)
+#Message on delete
 @client.event
 async def on_message_delete(message):
         if message.author == client.user:
                 return
 
-        await message.channel.send('{}: {}'.format(message.author.mention, message.content))
+        await message.channel.send('[DELETED]{}: {}'.format(message.author.mention, message.content))
                 
 client.run(str(os.environ.get('BOT_TOKEN')))
