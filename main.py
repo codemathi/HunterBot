539738abd79e6db7db5ac356f4ee79eff397e500
assil4 <51454567+assil4@users.noreply.github.com>
2019-06-07 13:27:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,39 +2,20 @@ import discord
 import os
 
 client = discord.Client()
-#Ready eveny
+
 @client.event
 async def on_ready():
     print('We heve logged in as {0.user}'.format(client))
     activity = discord.Game(name="Beta BOT v1")
     await client.change_presence(status=discord.Status.idle, activity=activity)
-#Message on delete
+    
 @client.event
-async def on_message_delete(message):
-        if message.author == client.user:
-            return
-        await message.channel.send('[DELETED]{}: {}'.format(message.author.mention, message.content))
 async def on_message(msg):
-    if message.author == client.user:
-        return
-        
-    try:
-        if msg.content.startwith('$!join'):
-                        await msg.channel.send('Trying')
-            if msg.voice_client is None:
-                                await msg.channel.send('Client')
-                if msg.author.voice:
-                                        await msg.channel.send('Connected')
-                    await msg.author.voice.channel.connect()
-                else:
-                    await msg.channel.send('You are not connected to a voice channel.')
-    except:
-        await msg.channel.send('Aint')
-@client.event
-async def on_member_join(self, member):
-    guild = member.guild
-    if guild.system_channel is not None:
-        to_send = 'Welcome {0.mention} to {1.name}!'.format(member, guild)
-        await guild.system_channel.send(to_send)
-        
+    if msg.content.startswith('.move'):
+        try:
+            vc = await msg.author.voice.channel.connect()
+            vc.play(discord.FFmpegPCMAudio(executable="C:/ffmreg/bin/ffmpeg.exe", source="C:/Users/Assil/Desktop/follow/testing.mp3"), after=lambda e: print('done', e), volume=100)
+            vc.is_playing()
+        except: print('Aint')   
+       
 client.run(str(os.environ.get('BOT_TOKEN')))
