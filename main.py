eca8ba24b222cd9720ba643ca892c8cbdb66c6ed
assil4 <51454567+assil4@users.noreply.github.com>
2019-06-12 11:36:10 +0100

    UPDATE

--- main.py
+++ main.py
@@ -1,20 +1,81 @@
 import discord
 import os
+import asyncore
+import re
+import sqlite3
+import cgi
+from collections import Counter
+from string import punctuation
+from math import sqrt
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from os import curdir, sep
 
 client = discord.Client()
 
+connection = sqlite3.connect('bot.sqlite')
+cursor = connection.cursor()
+
 @client.event
 async def on_ready():
-    print('We heve logged in as {0.user}'.format(client))
     activity = discord.Game(name="alpha")
     await client.change_presence(status=discord.Status.idle, activity=activity)
+
+def get_id(entityName, text):
+    tableName = entityName + 's'
+    columnName = entityName
+    cursor.execute('SELECT rowid FROM ' + tableName + ' WHERE ' + columnName + ' = ?', (text,))
+    row = cursor.fetchone()
+    if row:
+        return row[0]
+    else:
+        cursor.execute('INSERT INTO ' + tableName + ' (' + columnName + ') VALUES (?)', (text,))
+        return cursor.lastrowid
+ 
+def get_words(text):
+    wordsRegexpString = '(?:\w+|[' + re.escape(punctuation) + ']+)'
+    wordsRegexp = re.compile(wordsRegexpString)
+    wordsList = wordsRegexp.findall(text.lower())
+    return Counter(wordsList).items()
+
+def process_ui(H):    
+    words = get_words(B)
+    words_length = sum([n * len(word) for word, n in words])
+    sentence_id = get_id('sentence', H)
+    for word, n in words:
+        word_id = get_id('word', word)
+        weight = sqrt(n / float(words_length))
+        cursor.execute('INSERT INTO associations VALUES (?, ?, ?)', (word_id, sentence_id, weight))
     
+    connection.commit()
+    cursor.execute('CREATE TEMPORARY TABLE results(sentence_id INT, sentence TEXT, weight REAL)')
+    words = get_words(H)
+    words_length = sum([n * len(word) for word, n in words])
+    for word, n in words:
+        weight = sqrt(n / float(words_length))
+        cursor.execute('INSERT INTO results SELECT associations.sentence_id, sentences.sentence, ?*associations.weight/(4+sentences.used) FROM words INNER JOIN associations ON associations.word_id=words.rowid INNER JOIN sentences ON sentences.rowid=associations.sentence_id WHERE words.word=?', (weight, word,))
+    cursor.execute('SELECT sentence_id, sentence, SUM(weight) AS sum_weight FROM results GROUP BY sentence_id ORDER BY sum_weight DESC LIMIT 1')
+    row = cursor.fetchone()
+    cursor.execute('DROP TABLE results')
+    if row is None:
+        cursor.execute('SELECT rowid, sentence FROM sentences WHERE used = (SELECT MIN(used) FROM sentences) ORDER BY RANDOM() LIMIT 1')
+        row = cursor.fetchone()
+    B = row[1]
+    cursor.execute('UPDATE sentences SET used=used+1 WHERE rowid=?', (row[0],))
+    return B
+
+client.run(str(os.environ.get('BOT_TOKEN')))
+create_table_request_list = [
+	'CREATE TABLE words(word TEXT UNIQUE)',
+    'CREATE TABLE sentences(sentence TEXT UNIQUE, used INT NOT NULL DEFAULT 0)',
+    'CREATE TABLE associations (word_id INT NOT NULL, sentence_id INT NOT NULL, weight REAL NOT NULL)',
+]
 @client.event
 async def on_message(msg):
-    if msg.content.startswith('.move'):           
-        try:
-            await msg.author.voice.channel.connect()
-            await msg.channel.send("Channel: " + msg.channel.name + "\nJoining...")
-        except:
-            await msg.channel.send("{} Join to channel first".format(msg.author.mention))
-client.run(str(os.environ.get('BOT_TOKEN')))
+    if msg.author == client.self:
+    	return
+    process_ui(msg.content)
+
+for create_table_request in create_table_request_list:
+    try:
+        cursor.execute(create_table_request)
+    except: pass
